3b96fcec554c25df6b25572456560123b2a41efe
Irish Moore <jasonamoore@frontier.com>
2022-02-11 20:45:20 -0500

    setup the data parsing and the est route file

---
+++ routes/routes.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const { isModuleNamespaceObject } = require('util/types');
+
+module.exports = app => {
+    
+}
--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001
 
+// set data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+
+
 // set the listener
 app.listen(PORT, function() {
     console.log("Application running on PORT: " + PORT);
