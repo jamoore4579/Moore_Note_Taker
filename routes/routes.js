38bd7e8d26f6b3997bb8fc013391d84476fd5d9f
Irish Moore <jasonamoore@frontier.com>
2022-02-11 21:38:44 -0500

    routes are created

--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,30 @@ const path = require('path');
 const { isModuleNamespaceObject } = require('util/types');
 
 module.exports = app => {
-    
+
+    // notes database
+    fs.readFile("db/db.json","utf8", (err, data) => {
+
+        if (err) throw err;
+
+        let notes = JSON.parse(data);
+
+        // api/notes GET route
+        app.get("/api/notes", function(req, res) {
+            res.json(notes);
+        });
+
+        // api/notes POST route
+        app.post("/api/notes", function(req, res) {
+            let newNote = req.body;
+            notes.push(newNote);
+            updateDb();
+            return console.log("New note was added: "+newNote.title);
+        });
+
+        // retrieve note with specific ID
+        app.get("/api/notes/:id", function(req, res) {
+            res.json(notes[req.params.id]);
+        });
+    })
 }
